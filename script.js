8a890483fa648445d5a9bd7869eda75670467180
dieegomr <dmendes.rocha@gmail.com>
2022-08-10 16:51:25 -0300

    codigo refatorado

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const input = document.getElementById('input');
-const segmento = document.documentElement.style;
-const button = document.querySelector('.button');
+const input = document.querySelector('.input');
+const styleVariaveis = document.documentElement.style;
+const enviarButton = document.querySelector('.enviar_button');
 const displayContainer = document.querySelector('.container-display');
 const novaPartidaButton = document.querySelector('.nova-partida_button');
 let mensagem = document.querySelector('.mensagem');
 
-let estado = '#262A34';
+let estado = { sitacao: '', cor: '', motivo: 'ok' };
 let secretNumber = '';
-let corpadrao = '#dddddd';
-// let html = '';
+let corPadrao = '#dddddd';
 
 const HTML = `
 <div class="display-7seg display-centena">
@@ -46,153 +45,247 @@ const HTML3 = `
     <div class="segG-2 unidade"></div>
   </div`;
 
+const habilitarDesabilitaFormulario = () => {
+  input.classList.toggle('jogando');
+  input.classList.toggle('pausado');
+  enviarButton.classList.toggle('jogando');
+  enviarButton.classList.toggle('pausado');
+};
+
+// Condições iniciais do jogo
+
+const novaPartidaHandler = () => {
+  // Retira o estado correct da mensagem
+  mensagem.classList.remove(estado.motivo);
+
+  // Mudando estado para ok
+  estado.cor = '#262A34';
+  estado.sitacao = 'jogando';
+  estado.motivo = 'ok';
+
+  // Requisita um novo número
+  getSecretNumber();
+
+  // Coloca o zero na tela
+  renderizarNumeroTela('0');
+
+  // Limpa a mensagem
+  mensagem.textContent = '';
+
+  // Oculta o botão Nova Partida
+  novaPartidaButton.classList.toggle('hidden');
+
+  // Habilita formulário enviar
+  habilitarDesabilitaFormulario();
+};
+
+// Requisição do número secreto
 const getSecretNumber = async function () {
   try {
     const resp = await fetch(
       'https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300'
     );
-    if (!resp.ok) throw new Error(resp.status);
     console.log(resp);
+    // Confere se tem erro
+    if (!resp.ok) throw new Error(resp.status);
+
     const data = await resp.json();
+
+    // Armazena o numero secreto em uma variável
     secretNumber = data.value;
-    console.log('novo numero secreto');
   } catch (err) {
-    estado = 'rgba(204, 51, 0, 1)';
-    mensagem.classList.add('erro');
+    // Mudando estado para erro
+    estado.cor = 'rgba(204, 51, 0, 1)';
+    estado.sitacao = 'pausado';
+    estado.motivo = 'erro';
+
+    // Adiciona classe erro
+    mensagem.classList.add(estado.motivo);
+
+    // Altera a mensagem
     mensagem.textContent = 'ERRO';
+
+    // Renderiza botão nova partida
     novaPartidaButton.classList.toggle('hidden');
-    segmento.setProperty('--form-button-color', '#dddddd');
-    segmento.setProperty('--form-border-color', '#CFCFCF');
-    segmento.setProperty('--form-background-color', '#F5F5F5');
-    displayOnScreen(err.message);
+
+    // Desabilita formulário
+    habilitarDesabilitaFormulario();
+
+    // Renderiza erro na tela
+    renderizarNumeroTela(err.message);
   }
 };
 
-getSecretNumber();
-
-const eviarHandler = (event) => {
+// Ações ao enviar número
+const enviarHandler = (event) => {
   event.preventDefault();
-  if (estado === '#262A34') {
-    let enteredNumber = input.value;
-    checkSecretNumber(enteredNumber);
-    displayOnScreen(enteredNumber);
-    input.value = '';
-    console.log('secret', secretNumber);
-    console.log('cliquei');
-  }
-};
 
-const novaPartidaHandler = () => {
-  estado = '#262A34';
-  mensagem.classList.remove('correct');
-  getSecretNumber();
-  displayOnScreen('0');
-  mensagem.textContent = '';
-  novaPartidaButton.classList.toggle('hidden');
-  segmento.setProperty(
-    '--form-button-color',
-    'linear-gradient(180deg, #ef6c00 0%, #c0661c 100%)'
-  );
-  segmento.setProperty('--form-border-color', '#ff6600');
-  segmento.setProperty('--form-background-color', '#ffffff');
+  // Converir se o jogo esta jogável
+  if (estado.sitacao !== 'jogando') return;
+
+  // Armazena o valor digitado
+  let enteredNumber = input.value;
+
+  // Confere se o numero digitado é correto
+  checkSecretNumber(enteredNumber);
+
+  // Mostra o número na tela
+  renderizarNumeroTela(enteredNumber);
+
+  // Apaga informação do input
+  input.value = '';
+
+  ///////////////////////////////// APAGAR
+  console.log('secret', secretNumber);
+  console.log('cliquei');
+  //////////////////////////////////////////
 };
 
 // na saida, tenho um numero binario em formato de array
-const decimalParaBinario = (num) => {
-  let dec = parseInt(num);
-  let bin = dec.toString(2);
-  let arrBinarioStr = ('binario', bin.padStart(4, '0').split(''));
+const decimalParaBinario = (numeroString) => {
+  let numeroInteiro = parseInt(numeroString);
+  let numeroBinario = numeroInteiro.toString(2);
+  let arrBinarioStr = numeroBinario.padStart(4, '0').split('');
   return arrBinarioStr.map((str) => Number(str));
 };
 
-const binarioParaDisplay = (num) => {
-  const arr = decimalParaBinario(num);
+const converteDecimalPara7Seg = (numero) => {
+  const arr = decimalParaBinario(numero);
   let A = arr[3];
   let B = arr[2];
   let C = arr[1];
   let D = arr[0];
 
-  let seg_a = D || B || (!C && !A) || (C && A);
-  let seg_b = !C || (!B && !A) || (B && A);
-  let seg_c = C || !B + A;
-  let seg_d = D || (!C && !A) || (!C && B) || (B && !A) || (C && !B && A);
-  let seg_e = (!C && !A) || (B && !A);
-  let seg_f = D || (!B && !A) || (C && !B) || (C && !A);
-  let seg_g = D || (C && !B) || (C && !A) || (!C && B);
+  let aSegmento = D || B || (!C && !A) || (C && A);
+  let bSegmento = !C || (!B && !A) || (B && A);
+  let cSegmento = C || !B + A;
+  let dSegmento = D || (!C && !A) || (!C && B) || (B && !A) || (C && !B && A);
+  let eSegmento = (!C && !A) || (B && !A);
+  let fSegmento = D || (!B && !A) || (C && !B) || (C && !A);
+  let gSegmento = D || (C && !B) || (C && !A) || (!C && B);
 
-  return [seg_a, seg_b, seg_c, seg_d, seg_e, seg_f, seg_g];
+  return [
+    aSegmento,
+    bSegmento,
+    cSegmento,
+    dSegmento,
+    eSegmento,
+    fSegmento,
+    gSegmento,
+  ];
 };
 
-/* seletor: centena = 0, dezena = 1, unidade = 2*/
+enviarButton.addEventListener('click', enviarHandler);
+novaPartidaButton.addEventListener('click', novaPartidaHandler);
 
-const displayOn7Seg = (numBinario, seletor, estado) => {
-  numBinario.forEach((num, index) => {
-    if (num) {
-      segmento.setProperty(`--seg${index}-${seletor}-color`, `${estado}`);
+/* seletor: centena = 0, dezena = 1, unidade = 2*/
+const renderizarNumero7Seg = (numero, seletor, estado) => {
+  let numero7Seg = converteDecimalPara7Seg(numero);
+  numero7Seg.forEach((segmento, index) => {
+    if (segmento) {
+      styleVariaveis.setProperty(
+        `--seg${index}-${seletor}-color`,
+        `${estado.cor}`
+      );
     } else {
-      segmento.setProperty(`--seg${index}-${seletor}-color`, `${corpadrao}`);
+      styleVariaveis.setProperty(
+        `--seg${index}-${seletor}-color`,
+        `${corPadrao}`
+      );
     }
   });
 };
 
-const displayOnScreen = (enteredNumber) => {
-  const numero = enteredNumber.split('');
-  if (numero.length === 3) {
+// Retirar todos os displays de 7 segmentos da tela
+const retirarTodos7SegTela = () => {
+  while (displayContainer.firstChild) {
+    displayContainer.firstChild.remove();
+  }
+};
+
+// Renderiza números na tela dependendo do tamanho
+const renderizarNumeroTela = (enteredNumber) => {
+  const arrNumeroDigitado = enteredNumber.split('');
+
+  // Se for uma centena
+  if (arrNumeroDigitado.length === 3) {
     displayContainer.classList.remove('display1');
     displayContainer.classList.remove('display2');
     displayContainer.classList.add('display3');
-    while (displayContainer.firstChild) {
-      displayContainer.firstChild.remove();
-    }
+    retirarTodos7SegTela();
+
+    // Adicionar display de 7 segmentos na tela
     displayContainer.insertAdjacentHTML('beforeend', HTML);
     displayContainer.insertAdjacentHTML('beforeend', HTML2);
     displayContainer.insertAdjacentHTML('beforeend', HTML3);
-    numero.forEach((numero, index) => {
-      displayOn7Seg(binarioParaDisplay(numero), index, estado);
+
+    // Renderizar numero na tela
+    arrNumeroDigitado.forEach((numero, index) => {
+      renderizarNumero7Seg(numero, index, estado);
     });
-  } else if (numero.length === 2) {
+
+    // Se for uma dezena
+  } else if (arrNumeroDigitado.length === 2) {
     displayContainer.classList.remove('display1');
     displayContainer.classList.add('display2');
     displayContainer.classList.remove('display3');
-    while (displayContainer.firstChild) {
-      displayContainer.firstChild.remove();
-    }
+
+    retirarTodos7SegTela();
+
+    // Adicionar display de 7 segmentos na tela
     displayContainer.insertAdjacentHTML('beforeend', HTML);
     displayContainer.insertAdjacentHTML('beforeend', HTML2);
-    numero.forEach((numero, index) => {
-      displayOn7Seg(binarioParaDisplay(numero), index, estado);
+
+    // Renderizar numero na tela
+    arrNumeroDigitado.forEach((numero, index) => {
+      renderizarNumero7Seg(numero, index, estado);
     });
-  } else if (numero.length === 1) {
+
+    // Se for uma unidade
+  } else if (arrNumeroDigitado.length === 1) {
     displayContainer.classList.add('display1');
     displayContainer.classList.remove('display2');
     displayContainer.classList.remove('display3');
-    while (displayContainer.firstChild) {
-      displayContainer.firstChild.remove();
-    }
+
+    retirarTodos7SegTela();
+
+    // Adicionar display de 7 segmentos na tela
     displayContainer.insertAdjacentHTML('beforeend', HTML);
-    numero.forEach((numero, index) => {
-      displayOn7Seg(binarioParaDisplay(numero), index, estado);
+
+    // Renderizar numero na tela
+    arrNumeroDigitado.forEach((numero, index) => {
+      renderizarNumero7Seg(numero, index, estado);
     });
   }
 };
 
+// Compara o numero digitado com o numero secreto
 const compareToSecretNumber = (number) => {
   mensagem.textContent = secretNumber > Number(number) ? 'É maior' : 'É menor';
 };
 
 const checkSecretNumber = (number) => {
   if (secretNumber === Number(number)) {
-    estado = 'rgba(50, 191, 0, 1)';
-    mensagem.classList.add('correct');
+    // Muda estado
+    estado.cor = 'rgba(50, 191, 0, 1)';
+    estado.sitacao = 'pausado';
+    estado.motivo = 'ganhou';
+
+    // Adiciona o estado correct para a mensagem
+    mensagem.classList.add(estado.motivo);
+
+    // Altera texto da mensagem
     mensagem.textContent = 'Você acertou!!!!';
+
+    // Renderiza o botão nova partida
     novaPartidaButton.classList.toggle('hidden');
-    segmento.setProperty('--form-button-color', '#dddddd');
-    segmento.setProperty('--form-border-color', '#CFCFCF');
-    segmento.setProperty('--form-background-color', '#F5F5F5');
+
+    // Desabilita formulário
+    habilitarDesabilitaFormulario();
   } else if (secretNumber !== number) {
-    estado = '#262A34';
+    // Compara o numero com o numero secreto
     compareToSecretNumber(number);
   }
 };
 
-displayOnScreen('0');
+novaPartidaHandler();
